a0d31be474b93899dc1bb72eec0440f57ef02a0c
HimPaul <himadripaul.nitm@gmail.com>
2023-12-05 12:11:58 +0530

    new commit

--- force-app/main/default/lwc/forEachLwc/forEachLwc.js
+++ force-app/main/default/lwc/forEachLwc/forEachLwc.js
@@ -2,5 +2,39 @@ import { LightningElement } from 'lwc';
 
 export default class ForEachLwc extends LightningElement {
 
-    
+    employee = {
+
+        fname : 'Himadri',
+        lname : 'paul',
+        city : 'california',
+        age : 31
+    }
+
+    employeeList =[
+
+        {
+
+            fname : 'Haro',
+            lname : 'paul',
+            city : 'california',
+            age : 37
+        },
+
+        {
+
+            fname : 'Himadri',
+            lname : 'paul',
+            city : 'bangalore',
+            age : 31
+        },
+
+        {
+
+            fname : 'Max',
+            lname : 'glen',
+            city : 'perth',
+            age : 40
+        }
+
+    ]
 }
