7c96b6504971fc414a0ce6c74e485e708a9b96d3
HimPaul <himadripaul.nitm@gmail.com>
2023-12-25 16:42:39 +0530

    new commit

--- force-app/main/default/lwc/wireDecoratorExample1/wireDecoratorExample1.js
+++ force-app/main/default/lwc/wireDecoratorExample1/wireDecoratorExample1.js
@@ -1,6 +1,34 @@
-import { LightningElement } from 'lwc';
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
+import { LightningElement,api, wire} from 'lwc';
 
 export default class WireDecoratorExample1 extends LightningElement {
 
-    
+    @api recordId; //id of some perticular record
+
+    @wire (getRecord,{recordId:'$recordId',fields:['Account.Name','Account.Phone']})
+    accounts //accounts is variable name
+
+     get getName()
+     {
+       if(this.accounts.data)
+
+          return getFieldValue(this.accounts.data,'Account.Name');
+
+        else if(this.accounts.error)
+
+           return this.accounts.error;
+       
+     }
+
+     get getPhone()
+     {
+       if(this.accounts.data)
+
+          return getFieldValue(this.accounts.data,'Account.Phone');
+
+        else if(this.accounts.error)
+
+           return this.accounts.error;
+       
+     }
 }
