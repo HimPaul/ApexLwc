6f7c30a3829d89c928f22286a14727a09fe24b05
HimPaul <himadripaul.nitm@gmail.com>
2023-12-30 02:28:14 +0530

    new commit

--- force-app/main/default/lwc/demoCommunicationChildComponent/demoCommunicationChildComponent.js
+++ force-app/main/default/lwc/demoCommunicationChildComponent/demoCommunicationChildComponent.js
@@ -1,6 +1,12 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api,track } from 'lwc';
 
 export default class DemoCommunicationChildComponent extends LightningElement {
 
     @api passedName;
+    @api passedFull;
+    sirname;
+    sirnamehandler(sirname){
+        console.log("check please child");
+        this.sirname = sirname;
+    }
 }
--- force-app/main/default/lwc/demoCommunicationParentComponent/demoCommunicationParentComponent.js
+++ force-app/main/default/lwc/demoCommunicationParentComponent/demoCommunicationParentComponent.js
@@ -1,6 +1,26 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api,track } from 'lwc';
 
 export default class DemoCommunicationParentComponent extends LightningElement {
 
-    
+    name = 'Himadri paul';
+    sirname;
+    @track passedObj ={
+
+        name:"Anamika",
+        age:31
+    }
+
+    //Event happen on Parent Component
+    nameChangeHandler(event){
+        
+        this.name = event.target.value;
+    }
+    //ByCalling Child Component method in Parent Component
+    SirNameChangeHandler(event){
+        console.log("check please parent");
+        this.sirname = event.target.value;
+        console.log("this.sirname"+this.sirname);
+        const childComp = this.template.querySelector('c-demo-communication-child-component');
+        childComp.sirnamehandler(this.sirname);
+    }
 }
